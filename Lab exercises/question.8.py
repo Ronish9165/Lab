c63adcc1c93d746e8566444e1996927f1e30015f
Ronish <Ronish9165>
2021-03-09 12:14:44 +0545

    area of circle

---
+++ Lab exercises/question.8.py
@@ -0,0 +1,7 @@
+#write a pyhton program which accpets the radius of a circle from the user and compute the area.
+# #area of circle =(pi*r**2)
+
+radius=float(input("Enter the radius of circle:"))
+pi=22/7
+area_of_circle= (pi*radius**2)
+print(f'The radius of circle is {area_of_circle} sq units.')
