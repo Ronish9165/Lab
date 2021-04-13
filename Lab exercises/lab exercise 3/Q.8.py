111a931ecf6cc8d8df357c43f5a604a75609585b
Ronish <Ronish9165>
2021-04-13 12:05:01 +0545

    Sum of three digit

--- Lab exercises/lab exercise 3/Q.8.py
+++ Lab exercises/lab exercise 3/Q.8.py
@@ -1,3 +1,13 @@
 '''Given a three digit number.Find the sum of its digit.'''
-num=456
-last_digit=num%10
+
+def sum(n):
+    sum = 0
+    while (n != 0):
+        sum = sum + int(n % 10)
+        n= int(n/10)
+
+    return sum
+
+#Driver code
+n=int(input("Enter a three digit number:"))
+print(sum(n))
