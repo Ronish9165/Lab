735818f8e7620ed32e54fa657ced3630f5530ab4
Ronish <Ronish9165>
2021-04-13 11:18:56 +0545

    Print last digit

---
+++ Lab exercises/lab exercise 3/Q.6.py
@@ -0,0 +1,5 @@
+'''Given an integer number, print it's last digit.'''
+
+num=int(input('Enter the integer number:'))
+last_digit=num%10
+print(f'The last digit of given integer {num} is {last_digit}')
