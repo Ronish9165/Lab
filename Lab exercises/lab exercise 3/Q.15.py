f0a2a85b2e0b16a8353169019c56e2d1ea23884c
Ronish <Ronish9165>
2021-04-20 12:11:12 +0545

    Output of following questions

---
+++ Lab exercises/lab exercise 3/Q.15.py
@@ -0,0 +1,32 @@
+'''What will be the output of the following if a=2, b=3, c=4 d=3?'''
+
+a=2
+b=3
+c=4
+d=3
+
+#a a==b
+if a==b:
+    print('True')
+else:
+    print('False')
+
+#b a != d
+if a != d:
+    print('True')
+else:
+    print('False')
+
+#c b==d
+if b==d:
+    print('True')
+else:
+    print('False')
+
+#d a!=c
+#e a+=c
+#f b/=d
+#g b>d
+#h a<d
+#i b-a == c-b
+#j b>=d
