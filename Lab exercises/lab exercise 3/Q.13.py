7c54245d6f21dbc427e09314ba19d50264fa2aa8
Ronish <Ronish9165>
2021-04-20 11:54:45 +0545

    uppercase into lowercase

--- Lab exercises/lab exercise 3/Q.13.py
+++ Lab exercises/lab exercise 3/Q.13.py
@@ -1,2 +1,5 @@
-'''What is the output of 'APPLE'> 'apple' '''
+'''What is the output of 'APPLE' > 'apple' '''
 
+uppercase=str(input("Enter the uppercase letter apple:"))
+lower=uppercase.lower()
+print(f' The output of {uppercase} into lowercase is {lower}.')
